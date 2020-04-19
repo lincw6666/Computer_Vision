d3ceffc4a24ec45a4222cc227dd1a4a4154df725
scott870430 <scott19880525@gmail.com>
2020-04-19 22:47:53 +0800

    bilinear_upsampling2 complete

--- HW02/bilinear_upsampling2.py
+++ HW02/bilinear_upsampling2.py
@@ -70,9 +70,9 @@ def bilinear_upsampling(img, shape):
     # -----------------
     # | a | 1 | 2 | b |   
     # -----------------
-    # | 1 | 3 |   | 1 | 
+    # | 1 | 3 | 4 | 1 | 
     # -----------------
-    # | 2 |   |   | 2 |   
+    # | 2 | 5 | 6 | 2 |   
     # -----------------
     # | d | 1 | 2 | e |   
     # -----------------
@@ -80,93 +80,127 @@ def bilinear_upsampling(img, shape):
     ret_img[0:rdx+1:3, 0:rdy+1:3] = img[:x+1, :y+1]
 
     # case 1
-    ret_img[0:rdx+1:3, 1:rdy:3] = img[:x+1, :y] 
-    ret_img[0:rdx+1:3, 1:rdy:3] += img[:x+1, :y] 
+    ret_img[0:rdx+1:3, 1:rdy:3] = np.multiply(img[:x+1, :y], 2)
     ret_img[0:rdx+1:3, 1:rdy:3] += img[:x+1, 1:y+1] 
     ret_img[0:rdx+1:3, 1:rdy:3] = ret_img[0:rdx+1:3, 1:rdy:3] / 3
 
-    ret_img[1:rdx:3, 0:rdy+1:3,] = img[:x, :y+1]
-    ret_img[1:rdx:3, 0:rdy+1:3,] += img[:x, :y+1]
+    ret_img[1:rdx:3, 0:rdy+1:3,] = np.multiply(img[:x, :y+1], 2)
     ret_img[1:rdx:3, 0:rdy+1:3,] += img[1:x+1, :y+1]
     ret_img[1:rdx:3, 0:rdy+1:3,] = ret_img[1:rdx:3, 0:rdy+1:3,] / 3
 
     #case 2
     ret_img[0:rdx+1:3, 2:rdy:3] = img[:x+1, :y] 
-    ret_img[0:rdx+1:3, 2:rdy:3] += img[:x+1, 1:y+1] 
-    ret_img[0:rdx+1:3, 2:rdy:3] += img[:x+1, 1:y+1] 
+    ret_img[0:rdx+1:3, 2:rdy:3] += np.multiply(img[:x+1, 1:y+1], 2)
     ret_img[0:rdx+1:3, 2:rdy:3] = ret_img[0:rdx+1:3, 2:rdy:3] / 3
 
-    ret_img[2:rdx:3, 0:rdy+1:3,] = img[:x, :y+1]
-    ret_img[2:rdx:3, 0:rdy+1:3,] += img[1:x+1, :y+1]
-    ret_img[2:rdx:3, 0:rdy+1:3,] += img[1:x+1, :y+1]
-    ret_img[2:rdx:3, 0:rdy+1:3,] = ret_img[2:rdx:3, 0:rdy+1:3,] / 3
+    ret_img[2:rdx:3, 0:rdy+1:3] = img[:x, :y+1]
+    ret_img[2:rdx:3, 0:rdy+1:3] += np.multiply(img[1:x+1, :y+1], 2)
+    ret_img[2:rdx:3, 0:rdy+1:3] = ret_img[2:rdx:3, 0:rdy+1:3,] / 3
 
     #case 3
-    ret_img[1:rdx+1:3, 1:rdy:3] = np.multiply(2, ret_img[1:rdx+1:3, 0:rdy:3])
-    ret_img[1:rdx+1:3, 1:rdy:3] += ret_img[1:rdx+1:3, 3:rdy+1:3]
-    #ret_img[1:rdx+1:3, 1:rdy:3] += np.ret_img[0:rdx:3, 1:rdy+1:3]
-    #ret_img[1:rdx+1:3, 1:rdy:3] += ret_img[0:rdx:3, 1:rdy+1:3]
-    # Case 02 + Case 05:
-    # ---------------------
-    # |   |(a+b)/2|   | b |
-    # ------------------
-    # |   |       |   |   |
-    # ------------------
-    # |   |(c+d)/2|   | b |
-    # ------------------
-    # |   |       |   |   |
-    # ---------------------
-    #ret_img[1::2, ::2] = img    # This action includes case 05.
-    #ret_img[1:-1:2, ::2] += img[1:, :]
-    #ret_img[1:-1:2, ::2] = ret_img[1:-1:2, ::2] / 2
+    ret_img[1:rdx+1:3, 1:rdy:3] =  np.multiply(ret_img[1:rdx+1:3, 0:rdy:3], 2)   #left
+    ret_img[1:rdx+1:3, 1:rdy:3] += ret_img[1:rdx+1:3, 3:rdy+1:3]                 #right
+    ret_img[1:rdx+1:3, 1:rdy:3] += np.multiply(ret_img[0:rdx:3, 1:rdy+1:3], 2)   #top
+    ret_img[1:rdx+1:3, 1:rdy:3] += ret_img[3:rdx+1:3, 1:rdy+1:3]                 #down
+    ret_img[1:rdx+1:3, 1:rdy:3] =  ret_img[1:rdx+1:3, 1:rdy:3] / 6
 
-    # Case 03 + Case 06:
-    # -------------------------
-    # |       |   |       |   |
-    # -------------------------
-    # |(a+c)/2|   |(b+d)/2|   |
-    # -------------------------
-    # |       |   |       |   |
-    # -------------------------
-    # |   c   |   |   d   |   |
-    # -------------------------
-    #ret_img[::2, 1::2] = img    # This action includes case 06.
-    #ret_img[::2, 1:-1:2] += img[:, 1:]
-    #ret_img[::2, 1:-1:2] = ret_img[::2, 1:-1:2] / 2
+    #case 4
+    ret_img[1:rdx+1:3, 2:rdy:3] =  ret_img[1:rdx+1:3, 0:rdy:3]                   #left
+    ret_img[1:rdx+1:3, 2:rdy:3] += np.multiply(ret_img[1:rdx+1:3, 3:rdy+1:3], 2) #right
+    ret_img[1:rdx+1:3, 2:rdy:3] += np.multiply(ret_img[0:rdx:3, 2:rdy+1:3], 2)   #top
+    ret_img[1:rdx+1:3, 2:rdy:3] += ret_img[3:rdx+1:3, 2:rdy+1:3]                 #down
+    ret_img[1:rdx+1:3, 2:rdy:3] =  ret_img[1:rdx+1:3, 2:rdy:3] / 6
 
-    # Case 04 + Case 07 + Case 08 + Case 09:
-    # -----------------------------
-    # |   |           |   |       |
-    # -----------------------------
-    # |   |(a+b+c+d)/4|   |(c+d)/2|
-    # -----------------------------
-    # |   |           |   |       |
-    # -----------------------------
-    # |   |  (b+d)/2  |   |   d   |
-    # -----------------------------
-    #ret_img[1::2, 1::2] = img   # case 04, 07, 08, 09.
-    #ret_img[1:-1:2, 1::2] += img[1:, :] # case 04, 07.
-    #ret_img[1::2, 1:-1:2] += img[:, 1:] # case 04, 08.
-    #ret_img[1:-1:2, 1:-1:2] += img[1:, 1:]  # case 04.
-    #ret_img[1:-1:2, 1:-1:2] = ret_img[1:-1:2, 1:-1:2] / 4   # case 04.
-    #ret_img[1:-1:2, -1] = ret_img[1:-1:2, -1] / 2   # case 07.
-    #ret_img[-1, 1:-1:2] = ret_img[-1, 1:-1:2] / 2   # case 08.
+    #case 5
+    ret_img[2:rdx+1:3, 1:rdy:3] =  np.multiply(ret_img[2:rdx+1:3, 0:rdy:3], 2)   #left
+    ret_img[2:rdx+1:3, 1:rdy:3] += ret_img[2:rdx+1:3, 3:rdy+1:3]                 #right
+    ret_img[2:rdx+1:3, 1:rdy:3] += ret_img[0:rdx:3, 1:rdy+1:3]                   #top
+    ret_img[2:rdx+1:3, 1:rdy:3] += np.multiply(ret_img[3:rdx+1:3, 1:rdy+1:3], 2) #down
+    ret_img[2:rdx+1:3, 1:rdy:3] =  ret_img[2:rdx+1:3, 1:rdy:3] / 6
+
+    #case 6
+    ret_img[2:rdx+1:3, 2:rdy:3] =  ret_img[2:rdx+1:3, 0:rdy:3]   				 #left
+    ret_img[2:rdx+1:3, 2:rdy:3] += np.multiply(ret_img[2:rdx+1:3, 3:rdy+1:3], 2) #right
+    ret_img[2:rdx+1:3, 2:rdy:3] += ret_img[0:rdx:3, 2:rdy+1:3]                   #top
+    ret_img[2:rdx+1:3, 2:rdy:3] += np.multiply(ret_img[3:rdx+1:3, 2:rdy+1:3], 2) #down
+    ret_img[2:rdx+1:3, 1:rdy:3] =  ret_img[2:rdx+1:3, 1:rdy:3] / 6
+
+    #Case right up
+    # -------------
+    # | b | 3 | c |
+    # -------------
+    # | 1 | 3 | 1 |
+    # -------------
+    # | 2 | 3 | 2 |
+    # -------------
+    # | e | 3 | f |
+    # -------------
+
+    ret_img[:rdx+1:3, rdy+2::2] = img[:x+1, y+1:]
+
+    #case 1
+    ret_img[1:rdx+1:3, rdy+2::2] =  np.multiply(ret_img[0:rdx:3, rdy+2::2], 2) 	#top
+    ret_img[1:rdx+1:3, rdy+2::2] += ret_img[3:rdx+1:3, rdy+2::2]			   	#down
+    ret_img[1:rdx+1:3, rdy+2::2] =  ret_img[1:rdx+1:3, rdy+2::2] / 3
+
+    #case 2
+    ret_img[2:rdx+1:3, rdy+2::2] =  ret_img[0:rdx:3, rdy+2::2]					#top
+    ret_img[2:rdx+1:3, rdy+2::2] += np.multiply(ret_img[3:rdx+1:3, rdy+2::2], 2)#down
+    ret_img[2:rdx+1:3, rdy+2::2] =  ret_img[2:rdx+1:3, rdy+2::2] / 3
+
+    #case3
+    ret_img[:rdx, rdy+1::2] =  ret_img[:rdx, rdy:-1:2] 		#left
+    ret_img[:rdx, rdy+1::2] += ret_img[:rdx, rdy+2::2] 		#right
+    ret_img[:rdx, rdy+1::2] =  ret_img[:rdx, rdy+1::2] / 2
+
+    #Case left down
+    # -----------------
+    # | d | 1 | 2 | e |
+    # -----------------
+    # | 3 | 3 | 3 | 3 |
+    # -----------------
+    # | g | 1 | 2 | h |
+    # -----------------
+
+    ret_img[rdx+2::2, :rdy+1:3]  =  img[x+1:, :y+1]
+    #case 1
+    ret_img[rdx+2::2, 1:rdy+1:3] =  np.multiply(ret_img[rdx+2::2, :rdy:3], 2)   #left
+    ret_img[rdx+2::2, 1:rdy+1:3] += ret_img[rdx+2::2, 3:rdy+1:3]			   	#right
+    ret_img[rdx+2::2, 1:rdy+1:3] =  ret_img[rdx+2::2, 1:rdy+1:3] / 3
+
+    #case 2
+    ret_img[rdx+2::2, 2:rdy+1:3] =  ret_img[rdx+2::2, :rdy:3]					#left
+    ret_img[rdx+2::2, 2:rdy+1:3] += np.multiply(ret_img[rdx+2::2, 3:rdy+1:3], 2)#right
+    ret_img[rdx+2::2, 2:rdy+1:3] =  ret_img[rdx+2::2, 2:rdy+1:3] / 3
+
+    #case3
+    ret_img[rdx+1::2, :rdy+1:] =  ret_img[rdx:-1:2, :rdy+1:] 					#top
+    ret_img[rdx+1::2, :rdy+1:] += ret_img[rdx+2::2, :rdy+1:] 					#down
+    ret_img[rdx+1::2, :rdy+1:] =  ret_img[rdx+1::2, :rdy+1:] / 2
     print(ret_img.shape)
     print(ret_img)
     return ret_img.astype(np.uint8)
 
 
 if __name__ == '__main__':
-    #img = np.array([[1,10,100, 10, 1],[0.1, 0.01, 0.05, 0.01, 0.1], [1, 2, 3, 2, 1],[0.1, 0.01, 0.05, 0.01, 0.1], [1, 10, 100, 10, 1]])
-    #upsample_img = bilinear_upsampling(img, (11,11))
 
+    '''
     np.set_printoptions(suppress = True)
-    np.set_printoptions(precision = 3)   
-    img = np.array([[1,10,100, 10],[0.1, 0.01, 0.05, 0.01], [1, 2, 3, 2],[0.1, 0.01, 0.05, 0.01]])
+    np.set_printoptions(precision = 2)   
+    img = np.array([[1,10,50, 10, 1],[0.1, 0.01, 0.5, 0.01, 0.1], [1, 2, 3, 2, 1],[0.1, 0.01, 0.5, 0.01, 0.1], [1, 10, 50, 10, 1]])
+    #img = np.array([[1,10,100, 10],[0.1, 0.01, 0.05, 0.01], [1, 2, 3, 2],[0.1, 0.01, 0.05, 0.01]])
     print(img)
-    for x in range(8,10):
-        for y in range(8,10):
+    for x in range(10,12):
+        for y in range(10,12):
 
             upsample_img = bilinear_upsampling(img, (x, y))
+    '''
+    img = cv2.imread('./task1and2_hybrid_pyramid/1_bicycle.bmp')
+    print(img.shape)
+    upsample_img = bilinear_upsampling(img,(img.shape[0]*2, img.shape[1]*2 + 1, img.shape[2]))
+    print(upsample_img.shape)
+    cv2.imshow('Test', upsample_img)
+    cv2.waitKey(0)
+
     #cv2.imshow('Test', upsample_img)
     #cv2.waitKey(0)
