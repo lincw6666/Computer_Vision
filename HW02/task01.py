17b5113871c8af428397a9106e3e355ead1a3b92
paul12716 <58286701+paul12716@users.noreply.github.com>
2020-04-19 14:27:27 +0800

    Update task01.py

--- HW02/task01.py
+++ HW02/task01.py
@@ -24,6 +24,24 @@ def GaussianFilter(x , y , D0 , highpass = True):
 			] for i in range(x)
 		])
 
+def IdealFilter(x , y , D0 , highpass = True):
+	centerx = int(x/2) + 1 if x % 2 == 1 else int(x/2)
+	centery = int(y/2) + 1 if y % 2 == 1 else int(y/2)
+	if highpass == True:
+		return np.array([
+			[
+				0 if ((i-centerx)**2 + (j-centery)**2)**0.5 < D0 else 1\
+					for j in range(y)
+			] for i in range(x)
+		])
+	else:
+		return np.array([
+			[
+				0 if ((i-centerx)**2 + (j-centery)**2)**0.5 > D0 else 1\
+					for j in range(y)
+			] for i in range(x)
+		])
+	
 def ShiftImage(img):
 	shifted_img = img.copy()
 	shifted_img[::2, 1::2] = -img[::2, 1::2]
@@ -37,13 +55,21 @@ def Filter(img , mask):
 	filtered_f = shifted_f * mask
 	return ShiftImage(np.fft.ifft2(filtered_f))
 
-def Highpass(img , D0):
+def GaussianHighpass(img , D0):
 	x , y = img.shape
 	return Filter(img , GaussianFilter(x , y , D0 , highpass = True))
 
-def Lowpass(img , D0):
+def GaussianLowpass(img , D0):
 	x , y = img.shape
 	return Filter(img , GaussianFilter(x , y , D0 , highpass = False))
+
+def IdealHighpass(img , D0):
+	x , y = img.shape
+	return Filter(img , IdealFilter(x , y , D0 , highpass = True))
+
+def IdealLowpass(img , D0):
+	x , y = img.shape
+	return Filter(img , IdealFilter(x , y , D0 , highpass = False))
 
 def hybridImage(img1, img2, img1D0, img2D0):
 	highPassed = Highpass(img1, img1D0)
@@ -68,26 +94,41 @@ def Filter_Color(img , mask):
 	filter_back[:,:,2] = ShiftImage(np.fft.ifft2(filtered_f2))
 	return filter_back
 
-def Highpass_Color(img , D0):
+def GaussianHighpass_Color(img , D0):
 	x , y , z = img.shape
 	return Filter_Color(img , GaussianFilter(x , y , D0 , highpass = True))
 
-def Lowpass_Color(img , D0):
+def GaussianLowpass_Color(img , D0):
 	x , y , z = img.shape
 	return Filter_Color(img , GaussianFilter(x , y , D0 , highpass = False))
 
-def hybridImage_Color(img1, img2, img1D0, img2D0):
-	highPassed = Highpass_Color(img1, img1D0)
-	lowPassed = Lowpass_Color(img2, img2D0)
-	return highPassed + lowPassed
+def IdealHighpass_Color(img , D0):
+	x , y , z = img.shape
+	return Filter_Color(img , IdealFilter(x , y , D0 , highpass = True))
+
+def IdealLowpass_Color(img , D0):
+	x , y , z = img.shape
+	return Filter_Color(img , IdealFilter(x , y , D0 , highpass = False))
+
+def hybridImage_Color(img1, img2, img1D0, img2D0 , Gaussian = True):
+	if Gaussian == True:
+		highPassed = GaussianHighpass_Color(img1, img1D0)
+		lowPassed = GaussianLowpass_Color(img2, img2D0)
+		highPassed = highPassed.real.astype(np.uint8)
+		lowPassed = lowPassed.real.astype(np.uint8)
+		return highPassed + lowPassed
+	else:
+		highPassed = IdealHighpass_Color(img1, img1D0)
+		lowPassed = IdealLowpass_Color(img2, img2D0)
+		return highPassed + lowPassed
 
 img1 = cv2.imread("4_einstein.bmp" )
 img2 = cv2.imread("4_marilyn.bmp" )
 
-Highpass_image = Highpass_Color(img1 , 20)
-Lowpass_image = Lowpass_Color(img2 , 20)
+Highpass_image = GaussianHighpass_Color(img1 , 20)
+Lowpass_image = GaussianLowpass_Color(img2 , 20)
 
-hybrid = hybridImage_Color(img1, img2, 25, 10)
+hybrid = hybridImage_Color(img1, img2, 20 , 20 , Gaussian = True)
 
 cv2.imshow('hybrid' , hybrid.real.astype(np.uint8))
 cv2.waitKey(0)
