a33f0167d92564bd5db0a7741111b3949677c1f0
scott870430 <scott19880525@gmail.com>
2020-04-22 01:43:43 +0800

    update task02.py

--- HW02/task_02.py
+++ HW02/task_02.py
@@ -41,69 +41,87 @@ gaussian_kernel = np.exp(-( x**2 + y**2) )
 #Gaussian Filter Normalization
 gaussian_kernel = gaussian_kernel / gaussian_kernel.sum()
 
-if __name__ == '__main__':
-	for file in files:
-		gaussian_img = []
-		Laplacian_img = []
-		recovery_img = []
-		magnitude_spectrum_img = []
+for file in files:
+	gaussian_img = []
+	Laplacian_img = []
+	recovery_img = []
+	l_ms = []
+	g_ms = []
 
-		filename, extension = file.split('.')
-		img = cv2.imread(imgpath + '/' + file)
+	filename, extension = file.split('.')
+	img = cv2.imread(imgpath + '/' + file)
+	if isgray:
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).reshape(img.shape[0], img.shape[1], -1)
+
+	#calculate gaussian pyramid
+	for i in range(Pyramids_layer):
+		GF_img = convolution(img, gaussian_kernel) #Gaussian
+		gaussian_img.append(GF_img)
+
+		img = downsampling(GF_img) #downsampling
+
+	gaussian_img.append(img)
+	Laplacian_img.append(img)
+
+	#calculate Laplacian pyramid
+	for i in range(Pyramids_layer - 1, -1, -1):
+		Laplacian = gaussian_img[i] - bilinear_upsampling(img, gaussian_img[i].shape) #upsampling
+		Laplacian_img.append(Laplacian)
+		img = gaussian_img[i]
+
+	for e in Laplacian_img:
 		if isgray:
-			img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).reshape(img.shape[0], img.shape[1], -1)
+			#Fourier magnitude
+			dft = cv2.dft(np.float32(e), flags=cv2.DFT_COMPLEX_OUTPUT)
+			dft_shift = np.fft.fftshift(dft)
 
-		#calculate gaussian pyramid
-		for i in range(Pyramids_layer):
-			GF_img = convolution(img, gaussian_kernel) #Gaussian
-			gaussian_img.append(GF_img)
+			magnitude_spectrum = 20 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
+			(tempx, tempy) = magnitude_spectrum.shape
+			l_ms.append(magnitude_spectrum.reshape(tempx, tempy, 1))
 
-			img = downsampling(GF_img) #downsampling
-
-		gaussian_img.append(img)
-		Laplacian_img.append(img)
-
-		#calculate Laplacian pyramid
-		for i in range(Pyramids_layer - 1, -1, -1):
-			Laplacian = gaussian_img[i] - bilinear_upsampling(img, gaussian_img[i].shape) #upsampling
-			Laplacian_img.append(Laplacian)
-			img = gaussian_img[i]
-
-		for e in Laplacian_img:
-			if isgray:
-				#Fourier magnitude
-				dft = cv2.dft(np.float32(e), flags=cv2.DFT_COMPLEX_OUTPUT)
-				dft_shift = np.fft.fftshift(dft)
-
-				magnitude_spectrum = 20 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
-				(tempx, tempy) = magnitude_spectrum.shape
-				magnitude_spectrum_img.append(magnitude_spectrum.reshape(tempx, tempy, 1))
-		
-		#calculate recovery pyramid
-		for i in range(Pyramids_layer, 0, -1):
-			l_index = len(Laplacian_img) - i
-			
-			recovery = Laplacian_img[l_index]+ bilinear_upsampling(gaussian_img[i], Laplacian_img[l_index].shape)
-			recovery_img.append(recovery)
-
-		Laplacian_img.reverse()
-		recovery_img.reverse()
-		magnitude_spectrum_img.reverse()
-
-		#calculate magnitude spectrum
-		l = len(Laplacian_img)
-		for i in range(1, l+1):
-
-			plt.subplot(1, 2, 1),plt.imshow(Laplacian_img[i-1].reshape(Laplacian_img[i-1].shape[:2]), cmap = 'gray')
-			plt.title(''), plt.xticks([]), plt.yticks([])
-			plt.subplot(1, 2, 2),plt.imshow(magnitude_spectrum_img[i-1].reshape(magnitude_spectrum_img[i-1].shape[:2]))#, cmap = 'gray')
-			plt.title(''), plt.xticks([]), plt.yticks([])
-			plt.show()
-
-		image_pyramid(gaussian_img, filename + '_gaussian', isgray)
-		image_pyramid(Laplacian_img, filename + '_laplacian', isgray)
-		image_pyramid(recovery_img, filename + '_recovery', isgray)
-		
+	for e in gaussian_img:
 		if isgray:
-			image_pyramid(magnitude_spectrum_img, filename + '_spectrum', isgray)
+			#Fourier magnitude
+			dft = cv2.dft(np.float32(e), flags=cv2.DFT_COMPLEX_OUTPUT)
+			dft_shift = np.fft.fftshift(dft)
+
+			magnitude_spectrum = 20 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
+			(tempx, tempy) = magnitude_spectrum.shape
+			g_ms.append(magnitude_spectrum.reshape(tempx, tempy, 1))
+
+
+	recovery = Laplacian_img[0]
+	for i in range(1, len(Laplacian_img)):
+		recovery = Laplacian_img[i]+ bilinear_upsampling(recovery, Laplacian_img[i].shape)
+		recovery_img.append(recovery)
+
+	Laplacian_img.reverse()
+	recovery_img.reverse()
+	l_ms.reverse()
+	#calculate magnitude spectrum
+	l = len(Laplacian_img)
+	for i in range(1, l+1):
+
+		plt.subplot(1, 2, 1),plt.imshow(Laplacian_img[i-1].reshape(Laplacian_img[i-1].shape[:2]), cmap = 'gray')
+		plt.title(''), plt.xticks([]), plt.yticks([])
+		plt.subplot(1, 2, 2),plt.imshow(l_ms[i-1].reshape(l_ms[i-1].shape[:2]))#, cmap = 'gray')
+		plt.title(''), plt.xticks([]), plt.yticks([])
+		plt.savefig('./task02_Output/'+filename + '_laplacian_spectrum' + str(i) + '.png')
+
+	l = len(gaussian_img)
+	for i in range(1, l+1):
+
+		plt.subplot(1, 2, 1),plt.imshow(gaussian_img[i-1].reshape(gaussian_img[i-1].shape[:2]), cmap = 'gray')
+		plt.title(''), plt.xticks([]), plt.yticks([])
+		plt.subplot(1, 2, 2),plt.imshow(g_ms[i-1].reshape(g_ms[i-1].shape[:2]))#, cmap = 'gray')
+		plt.title(''), plt.xticks([]), plt.yticks([])
+		plt.savefig('./task02_Output/'+filename + '_gaussian_spectrum' + str(i) + '.png')
+
+	image_pyramid(gaussian_img, filename + '_gaussian', isgray)
+	image_pyramid(Laplacian_img, filename + '_laplacian', isgray)
+	image_pyramid(recovery_img, filename + '_recovery', isgray)
+	
+	if isgray:
+		image_pyramid(l_ms, filename + '_spectrum', isgray)
+
 
